b7c71719c8633f3ccbdbc0b6864d076f95456da1
Jeferson Mesquita <jeferson.mesquita@imobpower.com>
2022-07-27 13:44:53 -0300

    fix Navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { NativeBaseProvider, Box } from 'native-base';
 import { Loading } from './src/screens/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Loading />
+      <Routes />
     </NativeBaseProvider>
   );
 }
